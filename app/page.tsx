fdac10348b7f01b0cb89926b12cbcbf45bd6b758
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2026-04-15 15:34:03 +0530

    feat:update the grok issues

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-    <h1>hey sagar Chaurasia</h1>
-    <h1>Become the great enginner who can build anything | make people life easier</h1>
-    <button>Hire me</button>
-    <button>Yooo me</button>
-    <button>Yooop me</button>
-    <button>Yooop me</button>
-    <button>Yooop me</button>
-    <button>Yooop me</button>
-    <button>Yooop me</button>
-    <button>Yooop me</button>
+      <h1>Hey sagar you're great man</h1>
     </div>
   )
 }
